4b15b5e1d5fe3fe717b934dea1179564098b2556
Sadık Abdusselam Albayrak <s.abdusselam@gmail.com>
2023-12-26 14:39:55 +0300

    .

--- src/Battlefield.java
+++ src/Battlefield.java
@@ -1,12 +1,35 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author zeki1
- */
-public class Battlefield {
+public class Battlefield extends frmBattelfield{
+    private Character player;
+    private Character enemy;
+    public Battlefield(Character Player,Character Enemy) {
+        this.player = Player;
+        this.enemy  = Enemy;
+    }
+    
+    public void deneme(){
+        
+    }
+
+    
+    public Character getPlayer() {
+        return player;
+    }
+
+    
+    public void setPlayer(Character player) {
+        this.player = player;
+    }
+
+    
+    public Character getEnemy() {
+        return enemy;
+    }
+
+    
+    public void setEnemy(Character enemy) {
+        this.enemy = enemy;
+    }
+    
     
 }
