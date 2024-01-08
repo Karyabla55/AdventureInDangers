1d2e8897758841559324fef44a0ec3d04c1e5abf
Sadık Abdusselam Albayrak <s.abdusselam@gmail.com>
2024-01-08 16:50:59 +0300

    deneme

--- src/Enemy.java
+++ src/Enemy.java
@@ -18,6 +18,40 @@ public class Enemy extends Character{
         this.setMana(enemyMana[level]);
         this.setExperience(enemyExp[level]);
         this.setSkill(enemySkills[level]);
+    }
+    
+    private void setEnemies(){
+        enemyName.add("Vampir");
+        enemyName.add("Kurt Adam");
+        enemyName.add("Thonas");
+        enemyName.add("cadı");
+        
+        enemyHealth.add(100.0);
+        enemyHealth.add(150.0);
+        enemyHealth.add(200.0);
+        enemyHealth.add(75.0);
+        
+        enemyDamage.add(40.0);
+        enemyDamage.add(35.0);
+        enemyDamage.add(70.0);
+        enemyDamage.add(45.0);
+        
+        enemyGold.add(10);
+        enemyGold.add(20);
+        enemyGold.add(30);
+        enemyGold.add(40);
+        
+        enemyMana.add(10);
+        enemyMana.add(20);
+        enemyMana.add(30);
+        enemyMana.add(40);
+        
+        enemyExp.add(20);
+        enemyExp.add(30);
+        enemyExp.add(40);
+        enemyExp.add(15);
+        
+        
         
     }
     
